05079f25a19b39574f99ac84cf0cc62992f47e96
Sági György <sagi.gyorgy@w5.hu>
2018-06-04 10:59:58 +0200

    cookieg first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+CookieG cookie handling class based on CookieConsent.insites.com
+
+Inject your cookieconsent.insites.com based cookie-box with this JS class.
+
+Usage:
+<script src="src/cookieg.js" data-config="config/config.json"></script>
+Where config/config.json is your cookieconsent.insite.com generated configuration.
+In that JSON, CSS and SCRIPT properties are optional, they are used to specify the correct version of the original script.
---
+++ src/cookieg.js
@@ -0,0 +1,114 @@
+/**
+ * CookieG cookieconsent wrapper class
+ * @desc A cookieconsent.insites.com oszt�lyra �p�l� s�tikezel� oszt�ly.
+ * @desc Seg�ts�g�vel csak 1 f�jlt kell berakni a htmlbe, az �sszes t�bbi f�gg�s�get ez az oszt�ly int�z.
+ * @desc ha data-config attrib�tumban egy JSON f�jlt adunk meg akkor konfigur�ci�nak azt fogja haszn�lja, �gy tov�bb
+ * @desc egyszer�s�dik a k�d.
+ * @author György Sági (gyorgy.sagi@w5labs.com)
+ * @date 2018.05.29
+ * @version 0.1
+ */
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define('CookieG', [], factory);
+    } else {
+        // Browser globals
+        root.CookieG = factory(root);
+    }
+}
+(this, function (window) {
+
+    var currentScript = document.currentScript || (function() {
+            var scripts = document.getElementsByTagName('script');
+            return scripts[scripts.length - 1];
+        })();
+
+    var configPath = currentScript.getAttribute('data-config');
+
+    var loadJSON =function (callback, filePath) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', filePath, true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    };
+
+    function CookieG(options) {
+        this.cookieconsentURLs = {
+            'css': '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css',
+            'script': '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js'
+        };
+
+        if(options.hasOwnProperty('css')){
+            this.cookieconsentURLs.css = options.css;
+        }
+
+        if(options.hasOwnProperty('script')){
+            this.cookieconsentURLs.script = options.script;
+        }
+
+        this.cookieOptions = options;
+        this.init();
+    }
+
+    CookieG.prototype = {
+        'init': function () {
+            this.injectCSS(this.cookieconsentURLs.css);
+            this.injectScript(this.cookieconsentURLs.script);
+        },
+        'initCookieConsent': function () {
+            var self = this;
+            window.addEventListener("load", function () {
+                if (window.hasOwnProperty('cookieconsent')) {
+                    window.cookieconsent.initialise(self.cookieOptions);
+                } else {
+                    console.warn("Cookieconsent not detected");
+                }
+            });
+        },
+        'injectCSS': function (css, context) {
+            context = context || document.head;
+            var link = document.createElement('link');
+            link.type = 'text/css';
+            link.rel = 'stylesheet';
+            link.setAttribute('class', "cookiecss");
+            link.href = css;
+            context.appendChild(link);
+        },
+        'injectScript': function (src, d) {
+            var self = this;
+            d = d || document;
+            var script = d.createElement('script');
+            script.type = 'text/javascript';
+            script.async = true;
+            script.onload = function () {
+                self.initCookieConsent();
+            };
+            script.onerror = function () {
+                reject(src + ' cannot be added. (Not found, or script error.)');
+            };
+            script.setAttribute('class', "cookiescript");
+            script.src = src;
+            d.getElementsByTagName('head')[0].appendChild(script);
+        }
+    };
+
+    if(typeof configPath !== 'undefined'){
+        loadJSON(function(response){
+            try{
+                var jsonresponse = JSON.parse(response);
+                new CookieG(jsonresponse);
+            } catch (e) {
+                console.warn('Configuration file is not a JSON.')
+            }
+        }, configPath);
+    }
+
+    return CookieG;
+
+}));
